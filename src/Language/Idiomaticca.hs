56af8556018ddc059f42260ac961e544b10804ee
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-01-30 17:07:48 +0900

    Parse/translate regress/noinc/return_zero/main.c

--- src/Language/Idiomaticca.hs
+++ src/Language/Idiomaticca.hs
@@ -5,15 +5,23 @@ module Language.Idiomaticca
 import Language.ATS as ATS
 import Language.C as C
 
+statementTrans :: C.CStat -> ATS.Expression AlexPosn
+statementTrans (C.CCompound [] [C.CBlockStmt statement] _) = statementTrans statement
+statementTrans (C.CReturn (Just (C.CConst (C.CIntConst ret _))) _) = ATS.IntLit $ fromInteger $ C.getCInteger ret
+statementTrans _ = undefined
+
+extDeclTrans :: C.CExtDecl -> ATS.Declaration AlexPosn
+extDeclTrans (C.CFDefExt (C.CFunDef _ (C.CDeclr (Just funcIdent) _ _ _ _) _ statement _)) =
+  ATS.Impl Nothing (ATS.Implement
+                    undefined -- pos
+                    [] -- preUniversalsI
+                    [] -- implicits
+                    [] -- universalsI
+                    (ATS.Unqualified $ identToString funcIdent) -- nameI
+                    (Just []) -- iArgs
+                    (Right $ statementTrans statement)) -- _iExpression
+extDeclTrans _ = undefined
+
 interpretTranslationUnit :: C.CTranslUnit -> ATS.ATS AlexPosn
-interpretTranslationUnit cAst =
-  ATS.ATS [ATS.Impl
-             Nothing
-             (ATS.Implement
-              undefined -- pos
-              [] -- preUniversalsI
-              [] -- implicits
-              [] -- universalsI
-              (ATS.Unqualified "main") -- nameI
-              (Just []) -- iArgs
-              (Right $ ATS.IntLit 0))] -- _iExpression
+interpretTranslationUnit (C.CTranslUnit cDecls _) =
+  ATS.ATS $ fmap extDeclTrans cDecls
