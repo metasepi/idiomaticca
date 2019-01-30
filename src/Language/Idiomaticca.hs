2bb9aa0112bdf28787ed5167b56fcdd46955c0ce
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-01-30 19:08:04 +0900

    Add comment to note using corrode

--- src/Language/Idiomaticca.hs
+++ src/Language/Idiomaticca.hs
@@ -5,11 +5,13 @@ module Language.Idiomaticca
 import Language.ATS as ATS
 import Language.C as C
 
+-- Based on corrode/src/Language/Rust/Corrode/C.lhs#interpretStatement
 statementTrans :: C.CStat -> ATS.Expression AlexPosn
 statementTrans (C.CCompound [] [C.CBlockStmt statement] _) = statementTrans statement
 statementTrans (C.CReturn (Just (C.CConst (C.CIntConst ret _))) _) = ATS.IntLit $ fromInteger $ C.getCInteger ret
 statementTrans _ = undefined
 
+-- Based on corrode/src/Language/Rust/Corrode/C.lhs#perDecl
 extDeclTrans :: C.CExtDecl -> ATS.Declaration AlexPosn
 extDeclTrans (C.CFDefExt (C.CFunDef _ (C.CDeclr (Just funcIdent) _ _ _ _) _ statement _)) =
   ATS.Impl Nothing (ATS.Implement
@@ -22,6 +24,7 @@ extDeclTrans (C.CFDefExt (C.CFunDef _ (C.CDeclr (Just funcIdent) _ _ _ _) _ stat
                     (Right $ statementTrans statement)) -- _iExpression
 extDeclTrans _ = undefined
 
+-- Based on corrode/src/Language/Rust/Corrode/C.lhs#interpretTranslationUnit
 interpretTranslationUnit :: C.CTranslUnit -> ATS.ATS AlexPosn
 interpretTranslationUnit (C.CTranslUnit cDecls _) =
   ATS.ATS $ fmap extDeclTrans cDecls
