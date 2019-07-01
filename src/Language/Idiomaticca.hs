61b207dfcb466c1b459afed12200100f7d1c8812
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-01 13:54:16 +0900

    Simple name space for ATS

--- src/Language/Idiomaticca.hs
+++ src/Language/Idiomaticca.hs
@@ -2,51 +2,51 @@ module Language.Idiomaticca
     ( interpretTranslationUnit
     ) where
 
-import Language.ATS as ATS
+import Language.ATS as A
 import Language.C as C
 
-binop :: C.CBinaryOp -> ATS.Expression AlexPosn -> ATS.Expression AlexPosn -> ATS.Expression AlexPosn
+binop :: C.CBinaryOp -> A.Expression AlexPosn -> A.Expression AlexPosn -> A.Expression AlexPosn
 binop op lhs rhs = case op of
-  C.CMulOp -> ATS.Binary ATS.Mult lhs rhs
-  C.CDivOp -> ATS.Binary ATS.Div lhs rhs
+  C.CMulOp -> A.Binary A.Mult lhs rhs
+  C.CDivOp -> A.Binary A.Div lhs rhs
   -- xxx C.CRmdOp ...
-  C.CAddOp -> ATS.Binary ATS.Add lhs rhs
-  C.CSubOp -> ATS.Binary ATS.Sub lhs rhs
+  C.CAddOp -> A.Binary A.Add lhs rhs
+  C.CSubOp -> A.Binary A.Sub lhs rhs
   _ -> undefined
 
-interpretExpr :: C.CExpr -> ATS.Expression AlexPosn
+interpretExpr :: C.CExpr -> A.Expression AlexPosn
 interpretExpr (C.CConst const) = case const of
-  C.CIntConst int _ -> ATS.IntLit $ fromInteger $ C.getCInteger int
+  C.CIntConst int _ -> A.IntLit $ fromInteger $ C.getCInteger int
   _ -> undefined
 interpretExpr (C.CBinary op lhs rhs _) =
   binop op (interpretExpr lhs) (interpretExpr rhs)
 interpretExpr _ = undefined
 
-interpretStatement :: C.CStat -> ATS.Expression AlexPosn
+interpretStatement :: C.CStat -> A.Expression AlexPosn
 interpretStatement (C.CCompound [] [C.CBlockStmt statement] _) =
   interpretStatement statement
 interpretStatement (C.CReturn (Just expr) _) =
   interpretExpr expr
 interpretStatement _ = undefined
 
-interpretFunction :: CFunDef -> ATS.Declaration AlexPosn
+interpretFunction :: CFunDef -> A.Declaration AlexPosn
 interpretFunction (C.CFunDef _ (C.CDeclr (Just ident) _ _ _ _) _ body _) =
-  ATS.Impl Nothing -- implArgs
-           (ATS.Implement -- _impl
+  A.Impl Nothing -- implArgs
+           (A.Implement -- _impl
              undefined -- pos
              [] -- preUniversalsI
              [] -- implicits
              [] -- universalsI
-             (ATS.Unqualified $ identToString ident) -- nameI
+             (A.Unqualified $ identToString ident) -- nameI
              (Just []) -- iArgs
              (Right $ interpretStatement body)) -- _iExpression
 interpretFunction _ =
   undefined
 
-perDecl :: C.CExtDecl -> ATS.Declaration AlexPosn
+perDecl :: C.CExtDecl -> A.Declaration AlexPosn
 perDecl (C.CFDefExt f) = interpretFunction f
 perDecl _ = undefined
 
-interpretTranslationUnit :: C.CTranslUnit -> ATS.ATS AlexPosn
+interpretTranslationUnit :: C.CTranslUnit -> A.ATS AlexPosn
 interpretTranslationUnit (C.CTranslUnit cDecls _) =
-  ATS.ATS $ ATS.Include "\"share/atspre_staload.hats\"" : fmap perDecl cDecls
+  A.ATS $ A.Include "\"share/atspre_staload.hats\"" : fmap perDecl cDecls
