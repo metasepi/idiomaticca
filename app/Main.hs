79b04bb5a4e0b74d2050dfae1ec7fd97e7a499d5
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-02 11:29:42 +0900

    Add dumpc sub command

--- app/Main.hs
+++ app/Main.hs
@@ -17,24 +17,29 @@ wrapper = info (helper <*> versionInfo <*> command')
    <> header "idiomaticca - a tool to translate IDIOMATIC C into readable ATS")
 
 versionInfo :: Parser (a -> a)
-versionInfo = infoOption ("idiomaticca version: " ++ showVersion version) (short 'V' <> long "version" <> help "Show version")
+versionInfo = infoOption
+  ("idiomaticca version: " ++ showVersion version)
+  (short 'V' <> long "version" <> help "Show version")
 
 data Command = Trans { _file :: String }
              | DumpAts { _file :: String }
+             | DumpC { _file :: String }
 
 command' :: Parser Command
 command' = hsubparser
   (command "trans" (info trans (progDesc "Translate C into ATS"))
-  <> command "dumpats" (info dumpats (progDesc "Dump ATS's AST"))
+  <> command "dumpats" (info dumpats (progDesc "Dump AST of ATS code"))
+  <> command "dumpc" (info dumpc (progDesc "Dump AST of C code"))
   )
 
 trans :: Parser Command
-trans = Trans
-  <$> targetP mempty id "C filename"
+trans = Trans <$> targetP mempty id "C filename"
 
 dumpats :: Parser Command
-dumpats = DumpAts
-  <$> targetP mempty id "ATS filename"
+dumpats = DumpAts <$> targetP mempty id "ATS filename"
+
+dumpc :: Parser Command
+dumpc = DumpC <$> targetP mempty id "C filename"
 
 targetP :: Mod ArgumentFields String -> (Parser String -> a) -> String -> a
 targetP completions' f s = f
@@ -55,3 +60,6 @@ run (DumpAts file) = do
   atsSrc <- readFile file
   let Right atsAst = A.parse atsSrc
   putStrLn $ show atsAst
+run (DumpC file) = do
+  Right cAst <- C.parseCFilePre file
+  putStrLn $ show cAst
