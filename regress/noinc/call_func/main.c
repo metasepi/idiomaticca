9db086d1614dab2df95087aa8b6d9713eba7ce62
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-08 16:34:03 +0900

    Modify goal of regress/noinc/call_func

--- regress/noinc/call_func/main.c
+++ regress/noinc/call_func/main.c
@@ -1,9 +1,15 @@
+extern int func2(int, int);
+
 int func1(int a, int b) {
-	return a + b;
+	return func2(a, b);
 }
 
 int main() {
 	int r;
 	r = func1(1, 2);
 	return r - 3;
+}
+
+int func2(int a, int b) {
+	return a + b;
 }
