54d17daf786686f9673c344dc22952b987521d64
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-15 09:10:16 +0900

    New regress test: mix_char_int

---
+++ regress/noinc/mix_char_int/main.c
@@ -0,0 +1,16 @@
+int char_fib(char n) {
+	int f0 = 0, f1 = 1;
+
+	while (n > 0) {
+		n = n - 1;
+		int tmp = f1;
+		f1 = f0 + f1;
+		f0 = tmp;
+	}
+
+	return f0;
+}
+
+int main() {
+	return char_fib(10) - 55;
+}
