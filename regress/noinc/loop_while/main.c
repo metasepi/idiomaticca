cb15e90e6bdc025bd0bc57f13b41e412cfe9fad8
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-18 15:56:59 +0900

    Update regress test loop_while

--- regress/noinc/loop_while/main.c
+++ regress/noinc/loop_while/main.c
@@ -1,4 +1,4 @@
-int fib(int n) {
+int fib1(int n) {
 	int f0 = 0, f1 = 1;
 
 	while (n > 0) {
@@ -11,6 +11,18 @@ int fib(int n) {
 	return f0;
 }
 
+int fib2(int n) {
+	int f0 = 0, f1 = 1;
+
+	while (n-- > 0) {
+		int tmp = f1;
+		f1 = f0 + f1;
+		f0 = tmp;
+	}
+
+	return f0;
+}
+
 int main() {
-	return fib(10) - 55;
+	return fib1(10) - fib2(10);
 }
