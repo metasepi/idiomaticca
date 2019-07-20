42c777ba1db7d14db9199f600c6082b606af07e8
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-20 20:06:37 +0900

    Update regress

--- regress/noinc/loop_for_break/main.c
+++ regress/noinc/loop_for_break/main.c
@@ -1,16 +1,17 @@
 int for_break() {
-	int a = 10;
+	int a = 10, b = 10;
 
 	while(a < 20) {
 		a++;
 		if (a > 15) {
 			break;
 		}
+		b++;
 	}
 
-	return a;
+	return a + b;
 }
 
 int main() {
-	return for_break() - 16;
+	return for_break() - 16 - 15;
 }
