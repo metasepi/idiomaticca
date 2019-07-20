d06c678217b87a7745dc87f12679aa37b0f19cd7
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-20 09:35:22 +0900

    Easy regress test

--- regress/noinc/switch_case/main.c
+++ regress/noinc/switch_case/main.c
@@ -2,21 +2,12 @@ int switch_case(char c) {
 	int ret;
 
 	switch (c) {
-	case '0':
 	case '1':
 	case '2':
 	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
 		ret = 1;
 		break;
 	case ' ':
-	case '\n':
-	case '\t':
 		ret = 2;
 		break;
 	default:
@@ -30,7 +21,7 @@ int switch_case(char c) {
 int main() {
 	int num, space, other;
 
-	num = switch_case('0');
+	num = switch_case('1');
 	space = switch_case(' ');
 	other = switch_case('Q');
 
