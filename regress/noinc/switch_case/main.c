874e540ec97932c6e7f2d7fd5eaaba5f823545b2
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-07-20 09:10:41 +0900

    case without break

--- regress/noinc/switch_case/main.c
+++ regress/noinc/switch_case/main.c
@@ -3,9 +3,20 @@ int switch_case(char c) {
 
 	switch (c) {
 	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
 		ret = 1;
 		break;
 	case ' ':
+	case '\n':
+	case '\t':
 		ret = 2;
 		break;
 	default:
