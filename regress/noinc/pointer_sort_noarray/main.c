b1a799f7e99a4eb91e6a3751486f2423745d19fd
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-08-06 14:03:52 +0900

    New regress test: pointer_sort_noarray

---
+++ regress/noinc/pointer_sort_noarray/main.c
@@ -0,0 +1,30 @@
+void sort(int *pa, int *pb, int *pc) {
+	int temp, flag = 1;
+
+	while (flag == 1) {
+		flag = 0;
+		if (*pa > *pb) {
+			temp = *pa;
+			*pa = *pb;
+			*pb = temp;
+			flag = 1;
+		}
+		if (*pb > *pc) {
+			temp = *pb;
+			*pb = *pc;
+			*pc = temp;
+			flag = 1;
+		}
+	}
+}
+
+int main() {
+	int a = 3, b = 1, c = 2;
+	int *pa, *pb;
+
+	pa = &a;
+	pb = &b;
+	sort(pa, pb, &c);
+
+	return a * 100 + b * 10 + c * 1 - 123;
+}
