fa8a4705d109215c1eafe3c608c5b3ebf4215a9a
Kiwamu Okabe <kiwamu@debian.or.jp>
2019-08-07 15:11:04 +0900

    Modify regress test pointer_swap to fix #41

--- regress/noinc/pointer_swap/main.c
+++ regress/noinc/pointer_swap/main.c
@@ -1,4 +1,4 @@
-void swap(int *px, int *py) {
+void swap2(int *px, int *py) {
 	int temp;
 
 	temp = *px;
@@ -6,12 +6,16 @@ void swap(int *px, int *py) {
 	*py = temp;
 }
 
+void swap1(int *px, int *py) {
+	swap2(px, py);
+}
+
 int main() {
 	int x = 1, y = 2;
 	int *px;
 
 	px = &x;
-	swap(px, &y);
+	swap1(px, &y);
 
 	return x * 10 + y - 21;
 }
